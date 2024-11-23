a352e51cebcc3d06956026543a76b3ba1692f87d
bambutcha <stringdeity@gmail.com>
2024-11-23 23:09:09 +0300

    Edit Header.css About.css About.js Add shadows to header box, Close to complete "About" frame Add three success avatars
    [#frontend]

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -2,10 +2,10 @@
 import React from "react";
 import Header from "../../components/Header/Header";
 import "./About.css";
-import Yagol from "../../assets/men(4).png"; // Импорт изображения
-import Lavr from "../../assets/men(3).png"; // Импорт изображения
+import Yagol from "../../assets/Yagol.png"; // Импорт изображения
+import Lavr from "../../assets/Lavr.png"; // Импорт изображения
 import Odin from "../../assets/men(2).png"; // Импорт изображения
-import Greedas from "../../assets/men(1).png"; // Импорт изображения
+import Gredas from "../../assets/Gredas.png"; // Импорт изображения
 
 function About() {
   return (
@@ -16,28 +16,32 @@ function About() {
       </div>
       <div className="total-flex">
         <div className="cart">
-          <img className="img-cart" src={Odin} alt="Dima" />
+          <img className="img-cart" src={Yagol} alt="Yaga" />
           <div className="text-cart">
-            <p className="surname surnameO">Odintsov Dmitry Maksimovich</p>
+            <p className="surname surname1">Yagolnik Daniil Sergeevich</p>
             <p className="skilz">Back-end developer</p>
-            <pre className="linee">____________________________</pre>
-            <p className="skilz">Тел: +7 (919) 892 42-45</p>
-            <a href="#"><i className="fab fa-vk"></i></a>
-            <a href="#"><i className="fab fa-telegram"></i></a>
-            <a href="#"><i className="fas fa-envelope"></i></a>
+            <pre className="linee"></pre>
+            <p className="skilz number">Тел: +7 (919) 892 42-45</p>
+            <div class="social-icons">
+              <a href="#"><i class="fab fa-vk"></i></a>
+              <a href="#"><i class="fab fa-telegram"></i></a>
+              <a href="#"><i class="fas fa-envelope"></i></a>
+            </div>
           </div>
         </div>
         
         <div className="cart">
-          <img className="img-cart" src={Odin} alt="Dima" />
+          <img className="img-cart" src={Lavr} alt="Danya" />
           <div className="text-cart">
-            <p className="surname surnameO">Odintsov Dmitry Maksimovich</p>
+            <p className="surname surname2">Odintsov Dmitry Maksimovich</p>
             <p className="skilz">Back-end developer</p>
-            <pre className="linee">____________________________</pre>
-            <p className="skilz">Тел: +7 (919) 892 42-45</p>
-            <a href="#"><i className="fab fa-vk"></i></a>
-            <a href="#"><i className="fab fa-telegram"></i></a>
-            <a href="#"><i className="fas fa-envelope"></i></a>
+            <pre className="linee"></pre>
+            <p className="skilz number">Тел: +7 (919) 892 42-45</p>
+            <div class="social-icons">
+              <a href="#"><i class="fab fa-vk"></i></a>
+              <a href="#"><i class="fab fa-telegram"></i></a>
+              <a href="#"><i class="fas fa-envelope"></i></a>
+            </div>
           </div>
         </div>
       </div>
@@ -46,26 +50,30 @@ function About() {
         <div className="cart">
           <img className="img-cart" src={Odin} alt="Dima" />
           <div className="text-cart">
-            <p className="surname surnameO">Odintsov Dmitry Maksimovich</p>
+            <p className="surname surname3">Odintsov Dmitry Maksimovich</p>
             <p className="skilz">Back-end developer</p>
-            <pre className="linee">____________________________</pre>
-            <p className="skilz">Тел: +7 (919) 892 42-45</p>
-            <a href="#"><i className="fab fa-vk"></i></a>
-            <a href="#"><i className="fab fa-telegram"></i></a>
-            <a href="#"><i className="fas fa-envelope"></i></a>
+            <pre className="linee"></pre>
+            <p className="skilz number">Тел: +7 (919) 892 42-45</p>
+            <div class="social-icons">
+              <a href="#"><i class="fab fa-vk"></i></a>
+              <a href="#"><i class="fab fa-telegram"></i></a>
+              <a href="#"><i class="fas fa-envelope"></i></a>
+            </div>
           </div>
         </div>
         
         <div className="cart">
-          <img className="img-cart" src={Odin} alt="Dima" />
+          <img className="img-cart" src={Gredas} alt="Ilya" />
           <div className="text-cart">
-            <p className="surname surnameO">Odintsov Dmitry Maksimovich</p>
+            <p className="surname surname4">Odintsov Dmitry Maksimovich</p>
             <p className="skilz">Back-end developer</p>
-            <pre className="linee">____________________________</pre>
-            <p className="skilz">Тел: +7 (919) 892 42-45</p>
-            <a href="#"><i className="fab fa-vk"></i></a>
-            <a href="#"><i className="fab fa-telegram"></i></a>
-            <a href="#"><i className="fas fa-envelope"></i></a>
+            <pre className="linee"></pre>
+            <p className="skilz number">Тел: +7 (919) 892 42-45</p>
+            <div class="social-icons">
+              <a href="#"><i class="fab fa-vk"></i></a>
+              <a href="#"><i class="fab fa-telegram"></i></a>
+              <a href="#"><i class="fas fa-envelope"></i></a>
+            </div>
           </div>
         </div>
       </div>
