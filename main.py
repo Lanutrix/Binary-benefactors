510250961b81a84d3aadb27892374fedbcd28d37
lanutrix <dmodv@vk.com>
2024-11-24 14:29:31 +0300

    release

--- main.py
+++ main.py
@@ -7,7 +7,22 @@ from collections import deque
 import time
 import gen
 from multiprocessing import Process
+import json
 
+def get_blur_type(json_str):
+    """
+    Преобразует строку в JSON и извлекает значение по ключу 'blurType'.
+    
+    :param json_str: Строка в формате JSON
+    :return: Значение 'blurType' или сообщение об ошибке
+    """
+    try:
+        # Преобразуем строку в словарь
+        json_data = json.loads(json_str)
+        # Извлекаем значение по ключу 'blurType'
+        return json_data.get('blurType', 'Ключ "blurType" отсутствует в JSON')
+    except json.JSONDecodeError:
+        return 'Ошибка: Некорректный формат JSON'
 
 dp = []
 
@@ -41,9 +56,16 @@ def limit_requests():
             {"Content-Type": "application/json"},
         )
 
-def blur_video(type_file: str, name_object: str, grade_blur: int, path: str, output_path: str) -> bool:
+def blur_video(type_file: str, name_object: str, grade_blur: int, path: str, output_path: str, mask:str = '') -> bool:
     try:
-        jj = f'.\\XACK.exe {type_file} {name_object} {grade_blur} {path} {output_path}'
+        if name_object == 'face':
+            mod = "yolov8m-face.onnx yoloface.txt"
+        else:
+            mod = "yolo11m.onnx yoloclasses.txt"
+
+        
+        # .\XACK.exe changePhoto face 10 test_case2.jpg out_test_case2.jpg yolov8m-face.onnx yoloface.txt smile.jpeg
+        jj = f'.\\XACK.exe {type_file} {name_object} {grade_blur} {path} {output_path} {mod} smile.jpeg'
         os.system(jj)
         return True
     except Exception as e:
@@ -51,8 +73,8 @@ def blur_video(type_file: str, name_object: str, grade_blur: int, path: str, out
         return False
 
 
-def start_blur_video(type_file: str, name_object: str, grade_blur: int, path: str, output_path: str):
-    Process(target=blur_video, args=(type_file, name_object, grade_blur, path, output_path,), daemon=True).start()
+def start_blur_video(type_file: str, name_object: str, grade_blur: int, path: str, output_path: str, mask:str=''):
+    Process(target=blur_video, args=(type_file, name_object, grade_blur, path, output_path, mask,), daemon=True).start()
     return True
 
 
@@ -69,33 +91,40 @@ def upload_file():
 
         type_file = request.form.get('type_file')
         name_object = request.form.get('name_object')
+        filter = get_blur_type(request.form.get('filter'))
         grade_blur = request.form.get('grade_blur')
 
-        if not type_file or not name_object or not grade_blur:
+        if not type_file or not name_object or not filter:
             return jsonify({"message": "Missing required parameters"}), 400
-
-        try:
-            grade_blur = int(grade_blur)
-        except ValueError:
-            return jsonify({"message": "grade_blur must be an integer"}), 400
-
+        # filter = filter[""]
+        if filter == 'blur':
+            try:
+                grade_blur = int(grade_blur)
+            except ValueError:
+                return jsonify({"message": "grade_blur must be an integer"}), 400
+        else:
+            type_file = "change"+type_file[0].upper()+type_file[1:]
+            mask = "smile.jpeg"
+        print("xyi", type_file, name_object, grade_blur, filter)
         file_id = gen.generate_id(10)
         file_path = f"{file_id}_{file.filename}"
         output_path = f"output_{file_id}_{file.filename}"
         os.chdir('static')
         file.save(file_path)
-
+        mask = ''
         # time.sleep(0.1)
-        f =  start_blur_video(type_file, name_object, grade_blur, file_path, output_path)
+        f =  start_blur_video(type_file, name_object, grade_blur, file_path, output_path, mask)
         os.chdir('..')
         if f:
+            output_path = "http://192.168.206.53:8999/"+output_path
+            print('xer', output_path)
             dp.append(output_path)
             return jsonify({
-                "message": "File uploaded successfully",
                 "filename": output_path,
             }), 200
 
     except Exception as e:
+        print(f"An error occurred: {str(e)}")
         return jsonify({"message": f"An error occurred: {str(e)}"}), 500
 
 @app.route("/files/", methods=["GET"])
